90f63aa298c8df47158a0bd2aff4bbd07f338e8b
Leorio Paradinight <62891774+code-rgb@users.noreply.github.com>
2021-01-31 00:42:21 +0530

    bootstaps yeee

---
+++ player.js
@@ -0,0 +1,7 @@
+var player = new Clappr.Player({
+	source:
+		"https://storage.googleapis.com/valid-scene-301602/QT5I48K4TU9/23a_1611986780_70234.mp4",
+	poster: "http://clappr.io/poster.png",
+	mute: true,
+	parentId: "#player",
+});
