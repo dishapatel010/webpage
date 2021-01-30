7443a9a286083ab42038f8a841784a8a30278452
Leorio Paradinight <62891774+code-rgb@users.noreply.github.com>
2021-01-31 01:56:02 +0530

    Update player.js

--- player.js
+++ player.js
@@ -1,7 +1,22 @@
-var site = urlParams.get('url')
+function getSearchParameters() {
+    var prmstr = window.location.search.substr(1);
+    return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+}
+
+function transformToAssocArray( prmstr ) {
+    var params = {};
+    var prmarr = prmstr.split("&");
+    for ( var i = 0; i < prmarr.length; i++) {
+        var tmparr = prmarr[i].split("=");
+        params[tmparr[0]] = tmparr[1];
+    }
+    return params;
+}
+
+var params = getSearchParameters();
 
 var player = new Clappr.Player({
-	source: site,
+	source: params.url,
 	poster: "http://clappr.io/poster.png",
 	mute: true,
 	parentId: "#player",
