1505b887ba88fb7928fa1a491194faf2e8273188
Leorio Paradinight <62891774+code-rgb@users.noreply.github.com>
2021-01-31 16:50:00 +0530

    WIP (alpha)

--- player.js
+++ player.js
@@ -1,27 +1,61 @@
 function getSearchParameters() {
-    var prmstr = window.location.search.substr(1);
-    return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+	var prmstr = window.location.search.substr(1);
+	return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
 }
 
-function transformToAssocArray( prmstr ) {
-    var params = {};
-    var prmarr = prmstr.split("&");
-    for ( var i = 0; i < prmarr.length; i++) {
-        var tmparr = prmarr[i].split("=");
-        params[tmparr[0]] = tmparr[1];
-    }
-    return params;
+function transformToAssocArray(prmstr) {
+	var params = {};
+	var prmarr = prmstr.split("&");
+	for (var i = 0; i < prmarr.length; i++) {
+		var tmparr = prmarr[i].split("=");
+		params[tmparr[0]] = tmparr[1];
+	}
+	return params;
 }
 
 var params = getSearchParameters();
+const player = new Plyr("#player");
 
-var player = new Clappr.Player({
-	source: params.url,
-	poster: "https://i.imgur.com/uzKdTXG.jpg",
-	mute: true,
-	watermark: "https://raw.githubusercontent.com/code-rgb/USERGE-X/alpha/resources/ghosts.png",
-        position: 'top-left',
-        watermarkLink: "https://github.com/code-rgb/USERGE-X",
-	parentId: "#player",
-	
+//  ------------------Theme Toggle------------------
+var darkMode = document.getElementById("darkMode");
+var darkLabel = "darkModeLabel";
+var darkLang = "Dark";
+var lightLang = "Light";
+var imgID = "logo";
+
+window.addEventListener("load", function () {
+	if (darkMode) {
+		initTheme();
+		darkMode.addEventListener("change", function () {
+			resetTheme();
+		});
+	}
 });
+
+function initTheme() {
+	var darkThemeSelected =
+		localStorage.getItem("darkMode") !== null &&
+		localStorage.getItem("darkMode") === "dark";
+	darkMode.checked = darkThemeSelected;
+	darkThemeSelected
+		? document.body.setAttribute("data-theme", "dark")
+		: document.body.removeAttribute("data-theme");
+	darkThemeSelected
+		? (document.getElementById(darkLabel).innerHTML = darkLang)
+		: (document.getElementById(darkLabel).innerHTML = lightLang);
+}
+
+function resetTheme() {
+	if (darkMode.checked) {
+		document.body.setAttribute("data-theme", "dark");
+		localStorage.setItem("darkMode", "dark");
+		document.getElementById(imgID).src = darkImg;
+		document.getElementById(darkLabel).innerHTML = darkLang;
+	} else {
+		document.body.removeAttribute("data-theme");
+		localStorage.removeItem("darkMode");
+		document.getElementById(imgID).src = lightImg;
+		document.getElementById(darkLabel).innerHTML = lightLang;
+	}
+}
+//  ----------------------------------------------
